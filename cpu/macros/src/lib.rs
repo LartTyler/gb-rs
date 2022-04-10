488a2faa9aa308405c5c5f62cba3d7c4651e46cd
Tyler Lartonoix <tyler@lartonoix.com>
2022-04-10 01:26:11 -0400

    Clean up initial proc macros

--- cpu/macros/src/lib.rs
+++ cpu/macros/src/lib.rs
@@ -1,90 +1,89 @@
 use self::inputs::*;
-use self::instructions::load::{
-    LoadImmediateIntoR16Input, LoadImmediateIntoR8Input, LoadR16PointerIntoR8Input,
-    LoadR8IntoR8AllInput, LoadR8IntoR8Input,
-};
+use self::util::create_instruction_fn;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::parse_macro_input;
 
 mod inputs;
-mod instructions;
 mod registers;
 mod util;
 
 #[proc_macro]
 pub fn load_r8_into_r8(input: TokenStream) -> TokenStream {
-    let LoadR8IntoR8Input { src, dest } = parse_macro_input!(input as LoadR8IntoR8Input);
+    let R8IntoR8Input { src, dest } = parse_macro_input!(input as R8IntoR8Input);
 
     let fn_name = format_ident!("load_{}_into_{}", src.ident, dest.ident);
     let src = src.as_enum_expr();
     let dest = dest.ident;
 
-    TokenStream::from(quote! {
-        pub fn #fn_name(r: &mut crate::registers::Registers, _: &mut gb_rs_memory::Memory) -> crate::instructions::Effect {
+    TokenStream::from(create_instruction_fn(
+        fn_name,
+        quote! {
             load_byte_into_byte_register(r, #src, r.#dest)
-        }
-    })
+        },
+    ))
 }
 
 #[proc_macro]
 pub fn load_r8_into_r8_all(input: TokenStream) -> TokenStream {
-    let LoadR8IntoR8AllInput { dest } = parse_macro_input!(input as LoadR8IntoR8AllInput);
-    let dest = dest.ident;
+    let R8TargetInput { target } = parse_macro_input!(input as R8TargetInput);
+    let target = target.ident;
 
     TokenStream::from(quote! {
-        gb_rs_cpu_macros::load_r8_into_r8!(a, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(b, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(c, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(d, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(e, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(h, #dest);
-        gb_rs_cpu_macros::load_r8_into_r8!(l, #dest);
+        gb_rs_cpu_macros::load_r8_into_r8!(a, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(b, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(c, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(d, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(e, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(h, #target);
+        gb_rs_cpu_macros::load_r8_into_r8!(l, #target);
     })
 }
 
 #[proc_macro]
 pub fn load_immediate_into_r8(input: TokenStream) -> TokenStream {
-    let LoadImmediateIntoR8Input { dest } = parse_macro_input!(input as LoadImmediateIntoR8Input);
+    let R8TargetInput { target } = parse_macro_input!(input as R8TargetInput);
 
-    let fn_name = format_ident!("load_immediate_into_{}", dest.ident);
-    let dest = dest.as_enum_expr();
+    let fn_name = format_ident!("load_immediate_into_{}", target.ident);
+    let target = target.as_enum_expr();
 
-    TokenStream::from(quote! {
-        pub fn #fn_name(r: &mut crate::registers::Registers, m: &mut gb_rs_memory::Memory) -> crate::instructions::Effect {
-            load_immediate_into_byte_register(r, m, #dest)
-        }
-    })
+    TokenStream::from(create_instruction_fn(
+        fn_name,
+        quote! {
+            load_immediate_into_byte_register(r, m, #target)
+        },
+    ))
 }
 
 #[proc_macro]
 pub fn load_immediate_into_r16(input: TokenStream) -> TokenStream {
-    let LoadImmediateIntoR16Input { dest } = parse_macro_input!(input as LoadImmediateIntoR16Input);
+    let R16TargetInput { target } = parse_macro_input!(input as R16TargetInput);
 
-    let fn_name = format_ident!("load_immediate_into_{}", dest.ident);
-    let dest = dest.as_enum_expr();
+    let fn_name = format_ident!("load_immediate_into_{}", target.ident);
+    let target = target.as_enum_expr();
 
-    TokenStream::from(quote! {
-        pub fn #fn_name(r: &mut crate::registers::Registers, m: &mut gb_rs_memory::Memory) -> crate::instructions::Effect {
-            load_immediate_into_pair(r, m, #dest)
-        }
-    })
+    TokenStream::from(create_instruction_fn(
+        fn_name,
+        quote! {
+            load_immediate_into_pair(r, m, #target)
+        },
+    ))
 }
 
 #[proc_macro]
 pub fn load_r16_pointer_into_r8(input: TokenStream) -> TokenStream {
-    let LoadR16PointerIntoR8Input { src, dest } =
-        parse_macro_input!(input as LoadR16PointerIntoR8Input);
+    let R16IntoR8Input { src, dest } = parse_macro_input!(input as R16IntoR8Input);
 
     let fn_name = format_ident!("load_{}_pointer_into_{}", src.ident, dest.ident);
     let src = src.as_enum_expr();
     let dest = dest.as_enum_expr();
 
-    TokenStream::from(quote! {
-        pub fn #fn_name(r: &mut crate::registers::Registers, m: &mut gb_rs_memory::Memory) -> crate::instructions::Effect {
+    TokenStream::from(create_instruction_fn(
+        fn_name,
+        quote! {
             load_pair_pointer_into_byte_register(r, m, #src, #dest)
-        }
-    })
+        },
+    ))
 }
 
 #[proc_macro]
