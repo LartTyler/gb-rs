199c6f9d26ac4654404e5aefa7eb360c66765917
Tyler Lartonoix <tyler@lartonoix.com>
2022-12-01 18:25:24 -0500

    WIP

--- asm-cli/src/cli.rs
+++ asm-cli/src/cli.rs
@@ -5,4 +5,16 @@ use std::path::PathBuf;
 #[command(author, version, about)]
 pub struct Cli {
     pub file: PathBuf,
+
+    /// If provided, skip `offset` bytes before beginning disassembly.
+    #[arg(short, long, value_parser = clap_num::maybe_hex::<u16>)]
+    pub offset: Option<u16>,
+
+    /// If provided, do not enter interactive mode and output `limit` operations before
+    /// terminating.
+    #[arg(short, long, value_parser = clap_num::maybe_hex::<u16>)]
+    pub limit: Option<u16>,
+
+    #[arg(short, action = clap::ArgAction::Count)]
+    pub verbose: u8,
 }
--- asm-cli/src/main.rs
+++ asm-cli/src/main.rs
@@ -1,14 +1,42 @@
 use clap::Parser;
 use cli::Cli;
+use gb_rs_asm::operations::Operation;
+use gb_rs_asm::parse;
 use gb_rs_asm::sets::Instructions;
 use std::fs::File;
 use std::io::{self, Read, Write};
+use std::path::Path;
 
 mod cli;
 
 fn main() {
     let cli = Cli::parse();
-    let mut file = File::open(cli.file).expect("could not open input file");
+
+    if let Some(limit) = cli.limit {
+        non_interactive(cli, limit);
+    } else {
+        todo!("interactive mode not yet implemented");
+    }
+}
+
+fn non_interactive(cli: Cli, limit: u16) {
+    let mut disassembler = Disassembler::new(read_cart(&cli.file));
+    disassembler.jump(cli.offset.unwrap_or_default());
+
+    let printer = Printer::new(cli.verbose);
+
+    for _ in 0..limit {
+        let (pc, op) = match disassembler.next() {
+            Ok(op) => op,
+            Err(e) => panic!("{:?}", e),
+        };
+
+        printer.write((pc, &op));
+    }
+}
+
+fn read_cart(path: &Path) -> Vec<u8> {
+    let mut file = File::open(path).expect("could not open input file");
 
     let len = file.metadata().expect("could not get file metadata").len();
     let len: usize = len.try_into().expect("file too long");
@@ -17,24 +45,72 @@ fn main() {
     file.read_to_end(&mut data)
         .expect("could not read input file");
 
-    let instructions = Instructions::default();
-    let mut stack_pointer = 0x100; // Skip cart header
+    data
+}
 
-    loop {
-        let instr = match instructions.parse(&data, stack_pointer) {
-            Ok(i) => i,
-            Err(e) => panic!("{:?}", e),
-        };
+struct Disassembler {
+    data: Vec<u8>,
+    instructions: Instructions,
+    program_counter: u16,
+}
 
-        println!("{:?}", instr);
-        print!("{:#06X} | {}", stack_pointer, instr.kind);
+impl Disassembler {
+    pub fn new(data: Vec<u8>) -> Self {
+        Self {
+            data,
+            instructions: Instructions::default(),
+            program_counter: 0,
+        }
+    }
+
+    pub fn jump(&mut self, pc: u16) {
+        self.program_counter = pc;
+    }
+
+    pub fn next(&mut self) -> parse::Result<(u16, Operation)> {
+        let op = self.instructions.parse(&self.data, self.program_counter)?;
+        let pc = self.program_counter;
+
+        self.program_counter += op.width as u16;
+
+        Ok((pc, op))
+    }
+}
+
+struct Printer {
+    verbose: u8,
+}
+
+impl Printer {
+    pub fn new(verbose: u8) -> Self {
+        Self { verbose }
+    }
+
+    pub fn write<P: Printable>(&self, item: P) {
+        item.write(self.verbose);
+        println!("");
+
         io::stdout().flush().expect("could not flush stdout");
+    }
+}
 
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("error reading stdin");
+trait Printable {
+    fn write(&self, verbose: u8);
+}
 
-        stack_pointer += instr.width as u16;
+impl Printable for Operation {
+    fn write(&self, verbose: u8) {
+        if verbose == 0 {
+            print!("{}", self.kind);
+        } else {
+            print!("{:?}", self.kind);
+        }
+    }
+}
+
+impl Printable for (u16, &Operation) {
+    fn write(&self, verbose: u8) {
+        print!("{:#06X} | ", self.0);
+        self.1.write(verbose);
     }
 }
